64508a2590ccc0fc2cddebe369d43d2bf653c9b9
Rajat Rao <112152229+rajat1607@users.noreply.github.com>
2022-09-21 17:59:00 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1> Type Your Note</h1>
+      <p>Note Every Thought Of Yours So That You Never Forget Them</p>
+    </div>
+  );
+}
+
+export default Notes;
